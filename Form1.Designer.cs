f20890facdba1b99c4ee46ec6287ace1cc577b02
Даниил Рудь <rud@centrab.ru>
2023-03-28 09:02:09 +0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace SymmetricEncryptionAlgorithms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chartEncryption = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chartDecryption = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBoxSource = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxEncrypted = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxDecrypted = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.comboBoxMethods = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.comboBoxMode = new System.Windows.Forms.ComboBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.chartEncryption)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartDecryption)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(147, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(499, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Performance comparisons of symmetric encryption algorithms";
+            // 
+            // chartEncryption
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartEncryption.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartEncryption.Legends.Add(legend1);
+            this.chartEncryption.Location = new System.Drawing.Point(10, 302);
+            this.chartEncryption.Name = "chartEncryption";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartEncryption.Series.Add(series1);
+            this.chartEncryption.Size = new System.Drawing.Size(385, 264);
+            this.chartEncryption.TabIndex = 1;
+            this.chartEncryption.Tag = "";
+            this.chartEncryption.Text = "123";
+            // 
+            // chartDecryption
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chartDecryption.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chartDecryption.Legends.Add(legend2);
+            this.chartDecryption.Location = new System.Drawing.Point(403, 302);
+            this.chartDecryption.Name = "chartDecryption";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chartDecryption.Series.Add(series2);
+            this.chartDecryption.Size = new System.Drawing.Size(385, 264);
+            this.chartDecryption.TabIndex = 2;
+            this.chartDecryption.Text = "chart2";
+            // 
+            // textBoxSource
+            // 
+            this.textBoxSource.Location = new System.Drawing.Point(259, 52);
+            this.textBoxSource.Multiline = true;
+            this.textBoxSource.Name = "textBoxSource";
+            this.textBoxSource.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxSource.Size = new System.Drawing.Size(529, 115);
+            this.textBoxSource.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(476, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Source text";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(132, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Encrypted text";
+            // 
+            // textBoxEncrypted
+            // 
+            this.textBoxEncrypted.BackColor = System.Drawing.Color.White;
+            this.textBoxEncrypted.Location = new System.Drawing.Point(10, 211);
+            this.textBoxEncrypted.Multiline = true;
+            this.textBoxEncrypted.Name = "textBoxEncrypted";
+            this.textBoxEncrypted.ReadOnly = true;
+            this.textBoxEncrypted.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxEncrypted.Size = new System.Drawing.Size(385, 85);
+            this.textBoxEncrypted.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(553, 192);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Decrypted text";
+            // 
+            // textBoxDecrypted
+            // 
+            this.textBoxDecrypted.BackColor = System.Drawing.Color.White;
+            this.textBoxDecrypted.Location = new System.Drawing.Point(403, 211);
+            this.textBoxDecrypted.Multiline = true;
+            this.textBoxDecrypted.Name = "textBoxDecrypted";
+            this.textBoxDecrypted.ReadOnly = true;
+            this.textBoxDecrypted.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxDecrypted.Size = new System.Drawing.Size(385, 85);
+            this.textBoxDecrypted.TabIndex = 7;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 144);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(241, 23);
+            this.buttonStart.TabIndex = 9;
+            this.buttonStart.Text = "Start encryption process";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(132, 52);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(121, 23);
+            this.buttonOpen.TabIndex = 10;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // comboBoxMethods
+            // 
+            this.comboBoxMethods.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBoxMethods.FormattingEnabled = true;
+            this.comboBoxMethods.Location = new System.Drawing.Point(132, 82);
+            this.comboBoxMethods.Name = "comboBoxMethods";
+            this.comboBoxMethods.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxMethods.TabIndex = 11;
+            this.comboBoxMethods.SelectedIndexChanged += new System.EventHandler(this.comboBoxMethods_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(9, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Encryption method:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(9, 55);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "File for encryption:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(124, 302);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(98, 16);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Encryption time";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(515, 302);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(100, 16);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Decryption time";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(9, 120);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(87, 16);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Cipher mode:";
+            // 
+            // comboBoxMode
+            // 
+            this.comboBoxMode.Enabled = false;
+            this.comboBoxMode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBoxMode.FormattingEnabled = true;
+            this.comboBoxMode.Location = new System.Drawing.Point(132, 115);
+            this.comboBoxMode.Name = "comboBoxMode";
+            this.comboBoxMode.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxMode.TabIndex = 16;
+            this.comboBoxMode.SelectedIndexChanged += new System.EventHandler(this.comboBoxMode_SelectedIndexChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 173);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(776, 13);
+            this.progressBar1.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 562);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.comboBoxMode);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBoxMethods);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxDecrypted);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxEncrypted);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxSource);
+            this.Controls.Add(this.chartDecryption);
+            this.Controls.Add(this.chartEncryption);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Symmetric encryption algorithms";
+            ((System.ComponentModel.ISupportInitialize)(this.chartEncryption)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartDecryption)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartEncryption;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartDecryption;
+        private System.Windows.Forms.TextBox textBoxSource;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxEncrypted;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxDecrypted;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.ComboBox comboBoxMethods;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox comboBoxMode;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Diagnostics;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace SymmetricEncryptionAlgorithms
+{
+    public partial class Form1 : Form
+    {
+        private byte[] key;
+        private byte[] IV;
+        private byte[] cipherBytes;
+        List<CipherMode> cipherModesLocal = new List<CipherMode>() { CipherMode.ECB, CipherMode.CBC, CipherMode.CFB };
+
+        private Dictionary<string, long> dictionaryCipherResults;
+        private Dictionary<string, long> dictionaryDecipherResults;
+        public Form1()
+        {
+            InitializeComponent();
+
+            openFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+
+            string[] cipherTypes = { "All", "DES", "3DES", "RC2", "Rijndael", "AES" };
+            string[] cipherModes = { "ECB", "CBC", "CFB" };
+
+
+            comboBoxMethods.Items.AddRange(cipherTypes);
+            comboBoxMethods.SelectedIndex = 0;
+            comboBoxMode.Items.AddRange(cipherModes);
+
+            chartEncryption.Series.Clear();
+            chartDecryption.Series.Clear();
+
+            for (int i = 1; i < cipherTypes.Length; i++)
+            {
+                chartEncryption.Series.Add(cipherTypes[i]);
+                chartEncryption.Series[cipherTypes[i]].XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Auto;
+                chartEncryption.Series[cipherTypes[i]].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+
+                chartDecryption.Series.Add(cipherTypes[i]);
+                chartDecryption.Series[cipherTypes[i]].XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Auto;
+                chartDecryption.Series[cipherTypes[i]].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+            }
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() != DialogResult.Cancel)
+            {
+                string filename = openFileDialog1.FileName;
+                string fileText = File.ReadAllText(filename);
+                textBoxSource.Text = fileText;
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            SymmetricAlgorithm saDES = DES.Create();
+            SymmetricAlgorithm sa3DES = TripleDES.Create();
+            SymmetricAlgorithm saRC2 = RC2.Create();
+            SymmetricAlgorithm saRijndael = Rijndael.Create();
+            SymmetricAlgorithm saAES = Aes.Create();
+
+            switch (comboBoxMethods.SelectedItem.ToString())
+            {
+                case "All":
+                    RunCipher(new List<SymmetricAlgorithm> { saDES, sa3DES, saRC2, saRijndael, saAES }, cipherModesLocal, new List<string> { "DES", "3DES", "RC2", "Rijndael", "AES" });
+                    break;
+                case "DES":
+                    RunCipher(new List<SymmetricAlgorithm> { saDES }, cipherModesLocal, new List<string> { "DES" });
+                    break;
+                case "3DES":
+                    RunCipher(new List<SymmetricAlgorithm> { sa3DES }, cipherModesLocal, new List<string> { "3DES" });
+                    break;
+                case "RC2":
+                    RunCipher(new List<SymmetricAlgorithm> { saRC2 }, cipherModesLocal, new List<string> { "RC2" });
+                    break;
+                case "Rijndael":
+                    RunCipher(new List<SymmetricAlgorithm> { saRijndael }, cipherModesLocal, new List<string> { "Rijndael" });
+                    break;
+                case "AES":
+                    RunCipher(new List<SymmetricAlgorithm> { saAES }, cipherModesLocal, new List<string> { "AES" });
+                    break;
+            }
+        }
+
+        private void RunCipher(List<SymmetricAlgorithm> symmetricAlgorithm, List<CipherMode> cipherModesLocal, List<string> symmetricAlgorithmList)
+        {
+            dictionaryCipherResults = new Dictionary<string, long>();
+            dictionaryDecipherResults = new Dictionary<string, long>();
+            chartEncryption.Series.Clear();
+            chartDecryption.Series.Clear();
+            textBoxEncrypted.Clear();
+            textBoxDecrypted.Clear();
+            for (int i = 0; i < symmetricAlgorithm.Count; i++)
+            {
+                for (int j = 0; j < cipherModesLocal.Count; j++)
+                {
+                    progressBar1.PerformStep();
+                    Stopwatch stopwatch = new Stopwatch();
+                    stopwatch.Start();
+                    symmetricAlgorithm[i].GenerateKey();
+                    key = symmetricAlgorithm[i].Key;
+                    symmetricAlgorithm[i].GenerateIV();
+                    IV = symmetricAlgorithm[i].IV;
+                    symmetricAlgorithm[i].Mode = cipherModesLocal[j];
+                    symmetricAlgorithm[i].Padding = PaddingMode.PKCS7;
+
+                    MemoryStream ms = new MemoryStream();
+                    CryptoStream cs = new CryptoStream(ms, symmetricAlgorithm[i].CreateEncryptor(), CryptoStreamMode.Write);
+                    byte[] plainbytes = Encoding.UTF8.GetBytes(textBoxSource.Text.ToCharArray());
+                    cs.Write(plainbytes, 0, plainbytes.Length);
+                    cs.Close();
+                    cipherBytes = ms.ToArray();
+                    ms.Close();
+                    stopwatch.Stop();
+
+                    string ciphedText = Encoding.UTF8.GetString(cipherBytes);
+                    dictionaryCipherResults.Add(symmetricAlgorithmList[i] + " " + cipherModesLocal[j].ToString(), stopwatch.ElapsedTicks);
+                    textBoxEncrypted.Text += $"{symmetricAlgorithmList[i]} {cipherModesLocal[j]}: {ciphedText}\r\n";
+
+                    stopwatch = new Stopwatch();
+                    stopwatch.Start();
+                    symmetricAlgorithm[i].Key = key;
+                    symmetricAlgorithm[i].IV = IV;
+                    MemoryStream ms1 = new MemoryStream(cipherBytes);
+                    CryptoStream cs1 = new CryptoStream(ms1, symmetricAlgorithm[i].CreateEncryptor(), CryptoStreamMode.Read);
+                    byte[] plainbytes1 = new Byte[cipherBytes.Length];
+                    cs1.Read(plainbytes1, 0, cipherBytes.Length);
+                    cs1.Close();
+                    ms1.Close();
+                    stopwatch.Stop();
+
+                    string deciphedText = Encoding.UTF8.GetString(plainbytes);
+                    dictionaryDecipherResults.Add(symmetricAlgorithmList[i] + " " + cipherModesLocal[j], stopwatch.ElapsedTicks);
+                    textBoxDecrypted.Text += $"{symmetricAlgorithmList[i]} {cipherModesLocal[j]}: {deciphedText}\r\n";
+                }
+            }
+            int counter = 0;
+            foreach (KeyValuePair<string, long> item in dictionaryCipherResults)
+            {
+                chartEncryption.Series.Add(item.Key);
+                chartEncryption.Series[counter].Points.AddY(item.Value);
+                counter++;
+            }
+            counter = 0;
+            foreach (KeyValuePair<string, long> item in dictionaryDecipherResults)
+            {
+                chartDecryption.Series.Add(item.Key);
+                chartDecryption.Series[counter].Points.AddY(item.Value);
+                counter++;
+            }
+        }
+
+        private void comboBoxMethods_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxMethods.SelectedItem.ToString() != "All")
+            {
+                comboBoxMode.SelectedIndex = 0;
+            }
+            else
+            {
+                comboBoxMode.SelectedIndex = -1;
+            }
+        }
+
+        private void comboBoxMode_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cipherModesLocal.Clear();
+            comboBoxMode.Enabled = true;
+            switch (comboBoxMode.SelectedItem != null ? comboBoxMode.SelectedItem.ToString() : "All")
+            {
+                case "All":
+                    cipherModesLocal.Add(CipherMode.ECB);
+                    cipherModesLocal.Add(CipherMode.CBC);
+                    cipherModesLocal.Add(CipherMode.CFB);
+                    comboBoxMode.SelectedIndex = -1;
+                    comboBoxMode.Text = "";
+                    comboBoxMode.Enabled = false;
+                    break;
+                case "ECB":
+                    cipherModesLocal.Add(CipherMode.ECB);
+                    break;
+                case "CBC":
+                    cipherModesLocal.Add(CipherMode.CBC);
+                    break;
+                case "CFB":
+                    cipherModesLocal.Add(CipherMode.CFB);
+                    break; 
+            }
+        }
+    }
+}
